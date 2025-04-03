b1103a378a54fda9f52a8395e74ceeeb652b8ecf
Thomas HUYNH <thomashuynh795@gmail.com>
2025-04-03 15:15:54 +0200

    2025-04-03 13:15:54 UTC

---
+++ rent-car-api/src/test/java/esgi/rent_car_api/RentCarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package esgi.rent_car_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rent-properties-api/src/main/java/esgi/rent_properties_api/RentPropertiesApiApplication.java
@@ -0,0 +1,13 @@
+package esgi.rent_properties_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentPropertiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentPropertiesApiApplication.class, args);
+	}
+
+}
